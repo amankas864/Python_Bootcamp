548564f1d849eb8b4eb287dec04fb3a417507d8b
amankas864 <amankas864@gmail.com>
2022-05-18 21:08:03 +0530

    Day2_1

---
+++ Day_2/Day2_1.py
@@ -0,0 +1,9 @@
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+a=int(two_digit_number)%10
+b=int(int(two_digit_number)/10)
+print(a+b)
