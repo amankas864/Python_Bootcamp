96fd582b6dc5c3450112be6179b7f2e8a88fe9b3
amankas864 <amankas864@gmail.com>
2022-05-18 21:20:01 +0530

    Day2_3

---
+++ Day_2/Day2_3.py
@@ -0,0 +1,11 @@
+# 🚨 Don't change the code below 👇
+age = input("What is your current age?")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+d=(90-int(age))*365
+w=(90-int(age))*52
+m=(90-int(age))*12
+print(('You have {} days, {} weeks, and {} months left.').format(d,w,m))
+
+
